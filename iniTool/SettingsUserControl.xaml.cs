e1fb2622e3f213f4557c611286cdd1c93d3c6348
noelkra <37224232+noelkra@users.noreply.github.com>
2018-11-13 08:26:21 +0100

    Added confirmation dialog
    Added a confirmation dialog when the settings are saved and added a visual feedback for the onclick event.

--- iniTool/SettingsUserControl.xaml.cs
+++ iniTool/SettingsUserControl.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace iniTool
 {
@@ -30,6 +18,7 @@ namespace iniTool
             resEdit.SetModulesIniFile(tbModulesIniFile.Text);
             resEdit.SetRootModulesDir(tbRootModulesDir.Text);
             resEdit.SetRootSpecsDir(tbRootSpecsDir.Text);
+            MessageBox.Show("Settings saved!", "Successful", MessageBoxButton.OK, MessageBoxImage.Information);
         }
         private void settingsUserControl_Loaded(object sender, RoutedEventArgs e)
         {
